468ad4dacf26fd17dce401bf534dc4510dac28e9
Matthew Flanagan <mattimustang@gmail.com>
2012-05-29 05:09:52 +0000

    Fixed up desc and removed commented code.

--- lib/puppet/reports/kvlog.rb
+++ lib/puppet/reports/kvlog.rb
@@ -4,15 +4,12 @@ require 'yaml'
 Puppet::Reports.register_report(:kvlog) do
 
   desc <<-DESC
-  Send notification of failed reports in key=value format to syslog.
+  Send metrics in key=value format to syslog.
   DESC
 
   def process
-    Puppet.debug "Sending status for #{self.host} to syslog."
+    Puppet.debug "Sending metrics for #{self.host} to syslog."
     log = Puppet::Util::Log.create(:level => :info, :message => "", :source => self.host)
-    #log.level = :info
-    #log.source = "#{self.host}"
-    #log.message = ""
 
     self.metrics.each { |metric,data|
       data.values.each { |val|
